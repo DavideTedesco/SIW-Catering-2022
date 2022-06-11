78cce0233c0b21e44bb0a19cb8c490ba8d70eb74
Davide Tedesco <davide_hrd@hotmail.it>
2022-06-11 21:43:46 +0200

    Update CateringApplicationTests.java

--- Catering_SpringBoot/Catering/src/test/java/it/uniroma3/siw/catering/CateringApplicationTests.java
+++ Catering_SpringBoot/Catering/src/test/java/it/uniroma3/siw/catering/CateringApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CateringApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	//@Test
+	//void contextLoads() {
+	//}
 
 }
